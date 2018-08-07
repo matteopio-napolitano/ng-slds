5f520c0613e7a05d18d38f89e345810cf5e7bb6d
Matteo Pio Napolitano <matteopio.napolitano@gmail.com>
2018-08-07 18:19:47 +0200

    unit test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,17 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {AppHeaderComponent} from './app-header/app-header.component';
+import {AppTimelineComponent} from './app-timeline/app-timeline.component';
+import {AppCardComponent} from './app-card/app-card.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        AppHeaderComponent,
+        AppTimelineComponent,
+        AppCardComponent
       ],
     }).compileComponents();
   }));
@@ -12,16 +19,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'ng-slds'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-slds');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-slds!');
   }));
 });
